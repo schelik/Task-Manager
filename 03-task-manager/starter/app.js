7f1ebf5e2b47fe614a07e1ec8d40d31bca53549a
Semih Chelik <schelik@uwaterloo.ca>
2021-08-25 20:19:58 -0400

    final chagnes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,33 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config(); 
+
+
+//middleware
+app.use(express.static('./public'));
+app.use(express.json());
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send("Task manager app");
+})
+
+
+app.use('/api/v1/tasks', tasks);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Listening port ${port}...`));
+
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+start();
